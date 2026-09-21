0ca5ccdfe82cb9f3f2cbfc0e46c245c3b39e48c2
Kaii <wetramen99@gmail.com>
2026-09-21 20:06:17 +0800

    Add overtime pay for shifts over 8 hours - oledan.jillianbritney

--- shifts.js
+++ shifts.js
@@ -2,4 +2,14 @@ function isValidShift(hours) {
   return hours > 0 && hours <= 24;
 }
 
+function calculatePay(hours, rate) {
+  let pay;
+  if (hours > 8) {
+    pay = 8 * rate + (hours - 8) * rate * 1.5;
+  } else {
+    pay = hours * rate;
+  }
+  return Math.floor(pay);
+}
+
 module.exports = { isValidShift };
